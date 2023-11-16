f5a1e80eb160cd1c63a1eb7863c754e1cb43d4a8
chemitaro <iwasi_44@hotmail.com>
2023-11-16 14:17:20 +0900

    Refactor audio recording and conversion functions

--- s2t_whisper/main.py
+++ s2t_whisper/main.py
@@ -4,18 +4,19 @@ import sounddevice as sd
 import numpy as np
 import wave
 import threading
+from pydub import AudioSegment
 import sys
 from openai import OpenAI
+import os
 
-audio_file_path = "recording.wav"
+audio_file_path = "recording"
 
 
-def record_audio(wav_filename=audio_file_path, fs=44100, channels=1):
-    """ユーザーがエンターキーを押すまで録音を続け、ファイルに保存する関数."""
+def record_audio(filename=audio_file_path+".wav", fs=44100, channels=1):
+    """ユーザーがエンターキーを押すまで録音を続け、ファイルをOgg Vorbis形式で保存する関数."""
     # 録音用のグローバル変数
     global is_recording
     is_recording = True
-    wav_filename = "recording.wav"
 
     def record_internal():
         """内部で使用する録音処理関数."""
@@ -29,8 +30,7 @@ def record_audio(wav_filename=audio_file_path, fs=44100, channels=1):
             return frames
 
     def save_to_file(frames, filename):
-        """録音されたフレームをファイルに保存する関数."""
-        # データを連結して保存
+        """録音されたフレームをWAVファイルに保存する関数."""
         wav_data = np.concatenate(frames, axis=0)
         wav_file = wave.open(filename, 'wb')
         wav_file.setnchannels(channels)
@@ -38,10 +38,19 @@ def record_audio(wav_filename=audio_file_path, fs=44100, channels=1):
         wav_file.setframerate(fs)
         wav_file.writeframes(np.array(wav_data * 32767, dtype=np.int16))
         wav_file.close()
-        print(f"ファイル保存: {filename}")
+        print(f"WAVファイル保存: {filename}")
+
+    def convert_to_ogg(wav_filename):
+        """WAVファイルをOgg Vorbis形式に変換する関数."""
+        ogg_filename = wav_filename.replace(".wav", ".ogg")
+        audio = AudioSegment.from_wav(wav_filename)
+        audio.export(ogg_filename, format="ogg")
+        print(f"Oggファイル変換完了: {ogg_filename}")
+        os.remove(wav_filename)  # 元のWAVファイルを削除
+        return ogg_filename
 
     # 録音スレッドを開始
-    recording_thread = threading.Thread(target=lambda: save_to_file(record_internal(), wav_filename))
+    recording_thread = threading.Thread(target=lambda: save_to_file(record_internal(), filename))
     recording_thread.start()
 
     # エンターキー入力を待機
@@ -51,8 +60,11 @@ def record_audio(wav_filename=audio_file_path, fs=44100, channels=1):
     # 録音スレッドが終了するのを待つ
     recording_thread.join()
 
+    # WAVファイルをOgg Vorbis形式に変換
+    convert_to_ogg(filename)
 
-def convert_speech_to_text(file_path=audio_file_path, model="whisper-1", language="ja", temperature=0.0):
+
+def convert_speech_to_text(file_path=audio_file_path+".ogg", model="whisper-1", language="ja", temperature=0.0):
     """
     Convert an audio file to text using OpenAI's Whisper API.
 
@@ -62,13 +74,13 @@ def convert_speech_to_text(file_path=audio_file_path, model="whisper-1", languag
     :return: Transcribed text.
     """
     # Load the audio file
-    with open(file_path, "rb") as audio_file:
-        audio_data = audio_file.read()
+    # with open(file_path, "rb") as audio_file:
+    #     audio_data = audio_file.read()
     client = OpenAI()
 
     # Send the audio data to OpenAI's Whisper API
     transcript = client.audio.transcriptions.create(
-        file=audio_data,
+        file=open(file_path, "rb"),
         model=model,
         language=language,
         temperature=temperature
@@ -79,3 +91,6 @@ def convert_speech_to_text(file_path=audio_file_path, model="whisper-1", languag
 
 
 record_audio()
+text = convert_speech_to_text()
+
+print(text)
